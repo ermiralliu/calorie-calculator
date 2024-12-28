7821f3cb3de1c9294fa1f67d699b886b8301874e
ermiralliu <mirialliu@gmail.com>
2024-12-29 00:31:23 +0100

    added an extra method to not need the whole user object during foodEntry insertions

--- src/main/java/com/fti/softi/repositories/FoodEntryRepository.java
+++ src/main/java/com/fti/softi/repositories/FoodEntryRepository.java
@@ -15,6 +15,20 @@ import com.fti.softi.models.FoodEntry;
 public interface FoodEntryRepository extends JpaRepository<FoodEntry, Long> {
 	List<FoodEntry> findByUserId(Long userId);
 
-	@Query("SELECT SUM(fe.price) FROM FoodEntry fe WHERE user.id= :userId AND (fe.createdAt BETWEEN :startDate AND :endDate)") 
-	Double sumPriceByDateBetween(@Param("userId") Long userId, @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
+	@Query(value = "SELECT SUM(price) FROM food_entry WHERE user_id= :userId AND (created_at BETWEEN :startDate AND :endDate)", nativeQuery = true) 
+	Double sumPriceByDateBetween(
+    @Param("userId") Long userId, 
+    @Param("startDate") LocalDateTime startDate, 
+    @Param("endDate") LocalDateTime endDate
+  );
+
+  @Query(value = "INSERT INTO food_entry (user_id, name, description, price, calories, created_at) VALUES (:userIdd, :name, :description, :price, :calories, :createdAt)", nativeQuery = true) 
+  FoodEntry insertFoodEntrybyId(
+    @Param("userId") Long userId, 
+    @Param("name") String name, 
+    @Param("description") String description,
+    @Param("price") String price,
+    @Param("calories") String calories,
+    @Param("createdAt") LocalDateTime createdAt
+  );
 }
