2780c2d1834c33b7dd833b55e43df2df43907c28
ermiralliu <mirialliu@gmail.com>
2025-01-20 23:39:05 +0100

    removed stuff that's not in the requests

--- src/main/java/com/fti/softi/controllers/UserController.java
+++ src/main/java/com/fti/softi/controllers/UserController.java
@@ -33,13 +33,14 @@ public class UserController {
 			@RequestParam("username") String username,
 			@RequestParam("email") String email,
 			@RequestParam("password") String password,
-			@RequestParam("calorieLimit") Integer calorieLimit,
+			@RequestParam(name = "calorieLimit", required = false) Integer calorieLimit,
 			RedirectAttributes redirectAttributes
 	) {
 		if (userRepository.findByEmail(email) != null) {
 			redirectAttributes.addFlashAttribute("message", "Email is already in use");
 			return "redirect:/user/register";
 		}
+    calorieLimit = 2500;
 
 		var userRoles = Set.of(roleRepository.findByName("USER"));
 		var encryptor = new BCryptPasswordEncoder();
@@ -59,37 +60,37 @@ public class UserController {
 		return "redirect:/login";
 	}
 
-	@PutMapping
-	public String updateUser(
-			@RequestParam(name = "name", required = false) String name,
-			@RequestParam(name = "password", required = false) String password,
-			@RequestParam(name = "calorieLimit", required = false) String calorieLimit,
-			RedirectAttributes redirectAttributes
-	) {
-		Optional<User> optionalUser = currentUserService.getCurrentUser();
+	// @PutMapping
+	// public String updateUser(
+	// 		@RequestParam(name = "name", required = false) String name,
+	// 		@RequestParam(name = "password", required = false) String password,
+	// 		@RequestParam(name = "calorieLimit", required = false) String calorieLimit,
+	// 		RedirectAttributes redirectAttributes
+	// ) {
+	// 	Optional<User> optionalUser = currentUserService.getCurrentUser();
 
-		if (optionalUser.isPresent()) {
-			User user = optionalUser.get();
+	// 	if (optionalUser.isPresent()) {
+	// 		User user = optionalUser.get();
 
-			if (name != null) {
-				user.setName(name);
-			}
-			if (password != null) {
-				var encryptor = new BCryptPasswordEncoder();
-				user.setPassword(encryptor.encode(password));
-			}
-			if (calorieLimit != null) {
-				user.setDailyCalorieLimit(Integer.parseInt(calorieLimit));
-			}
+	// 		if (name != null) {
+	// 			user.setName(name);
+	// 		}
+	// 		if (password != null) {
+	// 			var encryptor = new BCryptPasswordEncoder();
+	// 			user.setPassword(encryptor.encode(password));
+	// 		}
+	// 		if (calorieLimit != null) {
+	// 			user.setDailyCalorieLimit(Integer.parseInt(calorieLimit));
+	// 		}
 
-			userRepository.save(user);
-		} else {
-			redirectAttributes.addFlashAttribute("message", "User not found.");
-			return "redirect:/error";
-		}
+	// 		userRepository.save(user);
+	// 	} else {
+	// 		redirectAttributes.addFlashAttribute("message", "User not found.");
+	// 		return "redirect:/error";
+	// 	}
 
-		return "redirect:/user";
-	}
+	// 	return "redirect:/user";
+	// }
 
 	@GetMapping
 	public String getUserView(Model model) {
